6dfacb40f682cfbb096415935182eef278b79afe
David Lyrek <dlyrek@wisc.edu>
2025-07-15 18:04:09 -0500

    Revert "Set up Supabase client with URL and anon key"
    This reverts commit c29e44c3d8b3906f3dc102a5ffd7fb2b734a4d6f.

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://jtjmzzjtfflcfjgednit.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0am16emp0ZmZsY2ZqZ2Vkbml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTI1NTksImV4cCI6MjA2ODE2ODU1OX0.fu0lXUIFWacsMgK8LHUaH3Y0QMpSriYDJ5bpmnQQVpg'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
